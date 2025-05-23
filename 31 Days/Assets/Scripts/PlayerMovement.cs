72bbc118f5dea8348d323a3f4a7a4b804fd24876
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2025-05-23 11:40:40 +0700

    Added Animation

--- 31 Days/Assets/Scripts/PlayerMovement.cs
+++ 31 Days/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,35 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed;
     private float movement;
+    private Animator anim;
+    [SerializeField] private float moveSpeed;
     [SerializeField] private InputActionReference move;
 
-    private void Awake() {
-        rb = GetComponent<Rigidbody2D>(); 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         move.action.Enable();
     }
 
-    private void Update() {
+    private void Update()
+    {
         movement = move.action.ReadValue<float>();
+
+        if (movement != 0)
+        {
+            Debug.Log("Moved!");
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         rb.linearVelocity = new Vector2(movement * moveSpeed, rb.linearVelocityY);
     }
 }

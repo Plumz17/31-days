42a0c9e81777dbd4f54fe65a5c9b7601f67cd361
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2025-08-07 23:13:11 +0700

    I Fuuked up

--- 31 Days/Assets/Scripts/RPG/Unit Data/EnemyData.cs
+++ 31 Days/Assets/Scripts/RPG/Unit Data/EnemyData.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "New Enemy", menuName = "RPG/EnemyData")]
 public class EnemyData : UnitData
 {
+    public int rarity;
     public Sprite enemySprite;
     public Sprite enemyInfoBox;
 }
